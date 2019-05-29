26d51920392c44f0a97d4d3a31ad1f497b659b02
Jasper Jiang <jasper.jiang@pwc.com>
2019-05-29 12:05:42 +0800

    update

--- src/_2_AddTwoNumbers.java
+++ src/_2_AddTwoNumbers.java
@@ -1,10 +1,25 @@
-package addTwoNumbers_2;
+//You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list. 
+//
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself. 
+//
+// Example: 
+//
+// 
+//Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//Output: 7 -> 0 -> 8
+//Explanation: 342 + 465 = 807.
+// 
+//
 
 /**
- * Created by Jack on 2018/6/30.
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
  */
-public class Solution {
-
+class _2_AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode result = new ListNode(0);
@@ -37,11 +52,6 @@ public class Solution {
     }
 
 
-    public static void main(String[] args) {
-
-    }
-
-
     public class ListNode {
         int val;
         ListNode next;
@@ -51,7 +61,3 @@ public class Solution {
         }
     }
 }
-
-
-
-
